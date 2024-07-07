6f7335710402439e872f01d9dc83e455b55d97c5
jeffjunohkim <166536100+jeffjunohkim@users.noreply.github.com>
2024-07-07 09:43:36 -0700

    updated engineering file

--- EmployeeSQL/data_engineering.sql
+++ EmployeeSQL/data_engineering.sql
@@ -1,39 +1,39 @@
 -- Creating the titles table
 CREATE TABLE titles (
     title_id CHAR(5) PRIMARY KEY,
-    title VARCHAR(40)
+    title VARCHAR(40) NOT NULL
 );
 
 -- Creating the departments table
 CREATE TABLE departments (
     dept_no CHAR(4) PRIMARY KEY,
-    dept_name VARCHAR(40)
+    dept_name VARCHAR(40) NOT NULL
 );
 
 -- Creating the employees table
 CREATE TABLE employees (
     emp_no INT PRIMARY KEY,
-    emp_title_id CHAR(5),
-    birth_date DATE,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    sex CHAR(1),
-    hire_date DATE,
+    emp_title_id CHAR(5) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    sex CHAR(1) NOT NULL,
+    hire_date DATE NOT NULL,
     FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 -- Creating the salaries table
 CREATE TABLE salaries (
-    emp_no INT,
-    salary INT,
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
     PRIMARY KEY (emp_no, salary),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 -- Creating the dept_emp table
 CREATE TABLE dept_emp (
-    emp_no INT,
-    dept_no CHAR(4),
+    emp_no INT NOT NULL,
+    dept_no CHAR(4) NOT NULL,
     PRIMARY KEY (emp_no, dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
@@ -41,8 +41,8 @@ CREATE TABLE dept_emp (
 
 -- Creating the dept_manager table
 CREATE TABLE dept_manager (
-    dept_no CHAR(4),
-    emp_no INT,
+    dept_no CHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
     PRIMARY KEY (dept_no, emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
