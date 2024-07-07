ad20da075cef914866c0f51f4bd40b61811ef98d
jeffjunohkim <166536100+jeffjunohkim@users.noreply.github.com>
2024-07-07 09:11:48 -0700

    homework stuff

---
+++ EmployeeSQL/data_engineering.sql
@@ -0,0 +1,49 @@
+-- Creating the titles table
+CREATE TABLE titles (
+    title_id CHAR(5) PRIMARY KEY,
+    title VARCHAR(40)
+);
+
+-- Creating the departments table
+CREATE TABLE departments (
+    dept_no CHAR(4) PRIMARY KEY,
+    dept_name VARCHAR(40)
+);
+
+-- Creating the employees table
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY,
+    emp_title_id CHAR(5),
+    birth_date DATE,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    sex CHAR(1),
+    hire_date DATE,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+-- Creating the salaries table
+CREATE TABLE salaries (
+    emp_no INT,
+    salary INT,
+    PRIMARY KEY (emp_no, salary),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- Creating the dept_emp table
+CREATE TABLE dept_emp (
+    emp_no INT,
+    dept_no CHAR(4),
+    PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+-- Creating the dept_manager table
+CREATE TABLE dept_manager (
+    dept_no CHAR(4),
+    emp_no INT,
+    PRIMARY KEY (dept_no, emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
